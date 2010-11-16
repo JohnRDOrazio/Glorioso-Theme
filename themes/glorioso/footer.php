a67bee1e9d0c72c2b25194a074d094308df7a44d
John R. D'Orazio <donjohn.fmmi@gmail.com>
2010-11-16 13:23:01 -0800

    simplify footer step two

--- themes/glorioso/footer.php
+++ themes/glorioso/footer.php
@@ -3,38 +3,38 @@ if ( strpos(strtolower($_SERVER['SCRIPT_NAME']),strtolower(basename(__FILE__))) 
 	header("Location: ../../index.php");
 	die("...");
 	}
-global $_FN;
 	?>
 
 <!-- FOOTER  -->
-<div id="pagebottom" style="font-size:.8em;">
-<span>
-<a href="http://flatnux.sourceforge.net"> <img style="vertical-align:middle;border:0px;" src="images/validate/flatnuke_powered.png"	alt="Powered by FlatNuX!" title="Powered by FlatNuX!" /></a>
-<a href="http://validator.w3.org/check/referer"> <img style="vertical-align:middle;border:0px;" src="images/validate/xhtml.png"	alt="Valid HTML 5!" title="Valid HTML 5!" /></a>
-<a href="http://jigsaw.w3.org/css-validator/check/referer"> <img style="vertical-align:middle;border:0px;" src="images/validate/valid_css.png" alt="Valid CSS!" title="Valid CSS!" /></a>
-<a href="<?php echo $_FN['datadir']?>/<?php	echo $_FN['lang']?>/backend.xml"> <img style="vertical-align:middle;border:0px;" src="images/validate/rss20_powered.png" alt="Get RSS 2.0 Feed"	title="Get RSS 2.0 Feed" /></a>
-<a href='http://jquery.com/' title='jQuery JavaScript Library'><img src='http://jquery.com/files/buttons/jquery-icon.png' alt='jQuery JavaScript Library' title='jQuery JavaScript Library' style='vertical-align:middle;border:none;' /></a>
-<br />
-Powered&nbsp;by&nbsp;
-<span style="font-weight:bold;"><a href="http://flatnux.sourceforge.net">FlatNuX</a></span>
-&nbsp;&copy;&nbsp;2003-2005
-&nbsp;|&nbsp;
-Site&nbsp;Admin:&nbsp;<a href="mailto:<?php echo $_FN['admin_mail']?>"><?php echo $_FN['admin']?></a>
-&nbsp;|&nbsp;
-<a href="<?php echo $_FN['datadir']?>/index.html">Full&nbsp;Map</a>
+<div id="pagebottom">
+  <div id="validate-icons">
+    <a class="validate-icon flatnux" href="http://flatnux.sourceforge.net"></a>
+    <a class="validate-icon html" href="http://validator.w3.org/check/referer"></a>
+    <a class="validate-icon css" href="http://jigsaw.w3.org/css-validator/check/referer"></a>
+    <a class="validate-icon rss" href="<?php echo $_FN['datadir']?>/<?php	echo $_FN['lang']?>/backend.xml"></a>
+    <a class="validate-icon jquery" href="http://jquery.com/" title="jQuery JavaScript Library"></a>
+    <a class="validate-icon jquery-ui" href="http://jquery-ui.com" title="jQuery UI"></a>
+  </div>
+  <div id="credits-links">
+  Powered&nbsp;by&nbsp;<a  style="font-weight:bold;" href="http://flatnux.sourceforge.net">FlatNuX</a>&nbsp;&copy;&nbsp;2003-2005
+  &nbsp;|&nbsp;
+  <span style="font-style:italic;">Glorioso Theme</span>
+  &nbsp;|&nbsp;
+  Site&nbsp;Admin:&nbsp;<a href="mailto:<?php echo $_FN['admin_mail']?>"><?php echo $_FN['admin']?></a>
+  &nbsp;|&nbsp;
+  <a href="<?php echo $_FN['datadir']?>/index.html">Full&nbsp;Map</a>
 <?php if ( file_exists("{$_FN['datadir']}/{$_FN['lang']}/backend.xml") ) { ?>
-&nbsp;|&nbsp;
-<a href="<?php echo $_FN['datadir']?>/<?php echo $_FN['lang']?>/backend.xml">Get&nbsp;RSS&nbsp;News</a>
+  &nbsp;|&nbsp;
+  <a href="<?php echo $_FN['datadir']?>/<?php echo $_FN['lang']?>/backend.xml">Get&nbsp;RSS&nbsp;News</a>
 <?php } ?>
-<br />
-<?php echo _LEGAL?>
-</span>
-
-
-	<span>
-		<?php $time2 = get_microtime();
-		echo "Page generated in ".sprintf("%.4f", abs(get_microtime() - $_FN['timestart']))." seconds.";
-		?>
-	</span>
+  </div>
+  <div id="credits-legal">
+    <?php echo _LEGAL?>
+  	<span>
+  		<?php $time2 = get_microtime();
+  		echo "Page generated in ".sprintf("%.4f", abs(get_microtime() - $_FN['timestart']))." seconds.";
+  		?>
+  	</span>
+  </div>
 </div>
 <!-- END FOOTER -->
