00c5fd6b4dee6c1eeb38071437fb42292b767d36
John R. D'Orazio <donjohn.fmmi@gmail.com>
2010-12-20 20:38:37 -0800

    true and false were backwards!

--- themes/glorioso/ajax/passcheck.php
+++ themes/glorioso/ajax/passcheck.php
@@ -1,12 +1,23 @@
-<?
-  global $_FN;
-  include_once("include/xmldb.php");
-  $db = new XMLDatabase("fndatabase","misc");
-
-  $pass = $_GET['password'];
-  $query = "SELECT passwd FROM users WHERE username = '{$_GET['username']}'";
-  $result = $db->Query($query);
-  if(md5($pass)==$result[0]['passwd']){ echo "false"; }
-  else{ echo "true"; }
-
+<?
+
+  global $_FN;
+
+  include_once("include/xmldb.php");
+
+  $db = new XMLDatabase("fndatabase","misc");
+
+
+
+  $pass = $_GET['password'];
+
+  $query = "SELECT passwd FROM users WHERE username = '{$_GET['username']}'";
+
+  $result = $db->Query($query);
+
+  if(md5($pass)==$result[0]['passwd']){ echo "true"; }
+
+  else{ echo "false"; }
+
+
+
 ?>
