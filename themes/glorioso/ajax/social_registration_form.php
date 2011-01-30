99fdbe756fef81487810fd4329ce91f9cb77eaab
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-01-29 20:03:11 -0800

    updated social registration form verification script

--- themes/glorioso/ajax/social_registration_form.php
+++ themes/glorioso/ajax/social_registration_form.php
@@ -1,33 +1,70 @@
 <script type="text/javascript">
-  function checkpass(thisform){
-      if (thisform=="linkoldaccount"){
-        if($("input#choice1").selected==false&&$("input#choice2").selected==false ){
-          $("div#msg",thisform).html("Mi dispiace ma devi scegliere quale username preferisci utilizzare.");
+
+$(document).ready(function(){
+  $("form").submit(function(){
+      $(".formcheck-icon").removeClass("ui-icon ui-icon-check ui-icon-alert");
+      $(".ui-state-error",$(this)).removeClass("ui-state-error");
+      $("#msg",$(this)).html("");
+      if ( $(this).attr("id")=="linkoldaccount"){
+        if($("#choice1").is(':checked')===false&&$("#choice2").is(':checked')===false ){
+          $("#choice1,#choice2").addClass("ui-state-error");
+          $("[for^='choice']").each(function(){
+            $(this).parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-alert");
+          }); 
+          $("#msg",$(this)).html("Devi scegliere quale username preferisci utilizzare.");
           return false;
         }
       }
-      if ( $("input#password","#"+thisform).val() == $("input#password2",thisform).val() ){
-        username = $("input#username","#"+thisform.val();
-        password = $("input#password","#"+thisform).val();
-        $.get("themes/glorioso/ajax/passcheck.php",{"username":username,"password":password}function(data){
-          if( data==="false" ) {
-            $("div#msg",thisform).html("Mi dispiace, la password non corrisponde a quella registrata per questo account! Riprova a digitare le password.")
-            return false;
-          }
-        });
-        $("div#socialregistration").dialog("close");
-        return true;
+      if ( $("#password",$(this)).val() == "" ){
+        $("#password",$(this)).addClass("ui-state-error").parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-alert");
+        $("#msg",$(this)).html("Password non può essere vuoto!");
+        return false;
       }
-      $("div#msg",thisform).html("Mi dispiace, le password non coincidono! Riprova a digitare le password.")
-      return false;
-  }
-$(document).ready(function(){
+      if ( $("#username",$(this)).val() == "" ){
+        $("#username",$(this)).addClass("ui-state-error").parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-alert");
+        $("#msg",$(this)).html("Username non può essere vuoto!");
+        return false;
+      }
+      if ( $("#password",$(this)).val() != $("#password2",$(this)).val() ){
+        $("[type='password']").each(function(){
+          $(this).addClass("ui-state-error").parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-alert");
+        }); 
+        $("#msg",$(this)).html("Le password non coincidono! Riprova a digitare le password.");
+        return false;
+      }
+      else{
+        $("[type='password']").each(function(){
+          $(this).parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-check");
+        }); 
+        var username = $("#username",$(this)).val(),
+            password = $("#password",$(this)).val(),
+            cont = false;
+        $.ajax({
+          url: "themes/glorioso/ajax/userpasscheck.php",
+          data: {"username":username,"password":password},
+          success: function(data){
+            if ( data=="not a user" ){
+              $("#username").addClass("ui-state-error").parent().next().children(".formcheck-icon").addClass("ui-icon ui-icon-alert");
+              $("#msg").html("Questo username non esiste! Si prega indicare uno username valido.");
+            }
+            else if ( data=="incorrect password" ) {
+              $("[type='password']:first").addClass("ui-state-error").parent().next().children(".formcheck-icon").removeClass("ui-icon-check").addClass("ui-icon-alert");
+              $("#msg").html("La password non è corretta per lo username indicato. Riprova a digitare la password.");
+            }
+            else if ( data=="password correct" ) {
+              //$("div#socialregistration").dialog("close");
+              cont = true;          
+            }
+          },
+          async: false
+        });
+        return cont;
+      }
+  });
+
   $("input#associatewithold","form#usernamealreadyexists").click(function(){
       $(".associatewithold").fadeIn("slow");
       $("button","form#usernamealreadyexists").fadeOut("slow").text("ASSOCIA ACCOUNT").fadeIn("slow");
-      $("form#usernamealreadyexists").submit(function(){
-        return checkpass("usernamealreadyexists");
-      });
   });
   $("input#alternativeusername","form#usernamealreadyexists").click(function(){
       $(".associatewithold").fadeOut("slow");
@@ -50,13 +87,37 @@ $(document).ready(function(){
   <span>Se vuoi collegarlo ad un account esistente, inserisci qui di seguito username e password del tuo account esistente.</span>
 </div>
 <div style="margin:15px;padding:10px;border:ridge 2px LightGray;">
-  <form id="linkoldaccount" name="linkoldaccount" action="index.php?spec=linkoldaccount" method="post" onsubmit="return checkpass('linkoldaccount')">
-  <div id="msg" style="text-align:center;color:#FF0000;"></div>
-    <label for="username">Username: </label><input type="text" name="username" id="username" /><br />
-    <label for="password">Password: </label><input type="password" name="password" id="password" /><br />
-    <label for="password2">Verifica password: </label><input type="password" name="password2" id="password2" /><br />
-    <input name="choiceusername" id="choice1" type="radio" value="preferfb" /><label for="choice1"> Preferisci username facebook, sovrascrivendo il vecchio username</label><br />
-    <input name="choiceusername" id="choice2" type="radio" value="preferold" /><label for="choice2"> Preferisci username account esistente, sovrascrivendo username di facebook</label><br />
+  <form id="linkoldaccount" name="linkoldaccount" action="index.php?spec=linkoldaccount" method="post">
+    <table>
+      <tr>
+        <td colspan=3 id="msg" style="text-align:center;color:#FF0000;height:15px;"></td>
+      </tr>
+      <tr>
+        <td><label for="username">Username: </label></td>
+        <td><input type="text" name="username" id="username" /></td>
+        <td><span class="formcheck-icon"></span></td>
+      </tr>
+      <tr>
+        <td><label for="password">Password: </label></td>
+        <td><input type="password" name="password" id="password" /></td>
+        <td><span class="formcheck-icon"></span></td>
+      </tr>
+      <tr>
+        <td><label for="password2">Verifica password: </label></td>
+        <td><input type="password" name="password2" id="password2" /></td>
+        <td><span class="formcheck-icon"></span></td>
+      </tr>
+      <tr>
+        <td><input name="choiceusername" id="choice1" type="radio" value="preferfb" /></td>
+        <td><label for="choice1"> Preferisci username facebook, sovrascrivendo il vecchio username</label></td>
+        <td><span class="formcheck-icon"></span></td>
+      </tr>
+      <tr>
+        <td><input name="choiceusername" id="choice2" type="radio" value="preferold" /></td>
+        <td><label for="choice2"> Preferisci username account esistente, sovrascrivendo username di facebook</label></td>
+        <td><span class="formcheck-icon"></span></td>
+      </tr>
+    </table>
     <div style="text-align:center;"><button type="submit">COLLEGA AD ACCOUNT ESISTENTE</button></div>
   </form>	
 </div>
@@ -75,7 +136,7 @@ $(document).ready(function(){
   <span>semplicemente inserendo la password:</span>
 </div>
 <div style="margin:15px;padding:10px;border:ridge 2px LightGray;">
-  <form id="existandsameusername" name="existandsameusername" action="index.php?spec=existandsameusername" method="post" onsubmit="return checkpass('existandsameusername')">
+  <form id="existandsameusername" name="existandsameusername" action="index.php?spec=existandsameusername" method="post">
   <div id="msg" style="text-align:center;color:#FF0000;"></div>
     <span>Devi immettere la password del tuo account esistente:</span><br /><br />
     <table>
@@ -89,7 +150,7 @@ $(document).ready(function(){
         <td><input type="password" name="password2" id="password2" /></td>
       </tr>
     </table>
-    <div style="text-align:center;"><button type="submit">COLLEGA ACCOUNT</button></div>
+    <div style="text-align:center;"><button>COLLEGA ACCOUNT</button></div>
   </form>	
 </div>
 <div style="text-align:center;">
@@ -143,7 +204,7 @@ $(document).ready(function(){
     echo "<div style='text-align:center;'>";
     echo "<div id=\"msg\" style=\"text-align:center;color:#FF0000;\"></div>";
     echo "<span>Hai chiesto di revocare le autorizzazioni di questa applicazione su facebook. Vuoi anche cancellare il tuo account su questo sito?</span>";
-    echo "<form id=\"revoke\" name=\"revoke\" method='POST' action=\"index.php?spec=deleteaccount\" onsubmit=\"return checkpass('revoke')\">";
+    echo "<form id=\"revoke\" name=\"revoke\" method='POST' action=\"index.php?spec=deleteaccount\">";
     echo "<label>USERNAME: <input type='text' name='username' /></label><br />";
     echo "<label>PASSWORD: <input type='password' name='password' /></label><br />";
     echo "<label>Verifica password: <input type='password' name='password2' /></label>";
