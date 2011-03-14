cccba33715960932d5322c15faf15107e8acc3a1
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-03-13 17:39:28 -0700

    added messenger live meta search tag

--- themes/glorioso/head.php
+++ themes/glorioso/head.php
@@ -52,7 +52,8 @@ switch($_FN['lang']){
 echo "\t".$htmlver['metachromeframe']."\n";
 echo "\t".$htmlver['metacharset']."\n";	
 echo "\t<meta http-equiv=\"Content-Language\" content=\""._FN_LANG."\" $close_tag>\n";
-	echo "\t<meta name=\"RESOURCE-TYPE\" content=\"DOCUMENT\" $close_tag>\n";
+echo $_THEME_CF["use_messlive"]==1 ? "<meta name=\"search.app\" content=\"WindowsLive\"/>" : "";
+echo "\t<meta name=\"RESOURCE-TYPE\" content=\"DOCUMENT\" $close_tag>\n";
 	echo "\t<meta http-equiv=\"EXPIRES\" content=\"0\" $close_tag>\n";
 	echo "\t<meta name=\"DISTRIBUTION\" content=\"GLOBAL\" $close_tag>\n";
 	echo "\t<meta name=\"AUTHOR\" content=\"{$_FN['sitename']}\" $close_tag>\n";
