1e003a32be397e857b1f984e859214777a2c81f4
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-03-19 06:49:11 -0700

    small typo

--- themes/glorioso/ajax/ajax_fc.php
+++ themes/glorioso/ajax/ajax_fc.php
@@ -3,7 +3,7 @@ include( $_SERVER['DOCUMENT_ROOT'] . "/include/flatnux.php" );
 //funzioni per le news
 $sctnews = find_section("news");
 require_once ('sections/' . $sctnews . '/functions.php');
-if(!function_exists("is_news_admin"){
+if(!function_exists("is_news_admin")){
      function is_news_admin(){
           return is_news_administrator();
      }
