6ff8fd6153501fbe02d5d22ac4c21fb89b643132
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-03-19 06:45:54 -0700

    latest updates to flatnux broke is_news_admin function, now conditional check for function in ajax_fc.php

--- themes/glorioso/ajax/ajax_fc.php
+++ themes/glorioso/ajax/ajax_fc.php
@@ -3,7 +3,11 @@ include( $_SERVER['DOCUMENT_ROOT'] . "/include/flatnux.php" );
 //funzioni per le news
 $sctnews = find_section("news");
 require_once ('sections/' . $sctnews . '/functions.php');
-//if (is_news_admin()) { echo "hello world!"; }
+if(!function_exists("is_news_admin"){
+     function is_news_admin(){
+          return is_news_administrator();
+     }
+}
 if (is_news_admin()) { echo "ijsynuwesvaedjm"; }
 else { echo "scnwogtunkwtsdavdjhm";  }
 ?>
