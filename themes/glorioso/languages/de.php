f7318dd28c80649abec733f3ee87c3e99d47b1ec
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-03-21 07:15:58 -0700

    added _FB_LOCALE, takes place of previous lcl variable

--- themes/glorioso/languages/de.php
+++ themes/glorioso/languages/de.php
@@ -3,6 +3,7 @@
     define("_GFC_LOGIN_BTN","Melden Sie sich mit Google");
     define("_GFC_LOGIN","Melden Sie sich bei {$_FN['sitename']} mit Google");
     define("_GFC_LOGOUT","Beenden {$_FN['sitename']} und Google Friend Connect");
+    define("_FB_LOCALE","de_DE");
     define("_FB_LOGIN","Melden Sie sich bei {$_FN['sitename']} mit Facebook Connect");
     define("_FB_LOGOUT","Beenden {$_FN['sitename']} und Facebook");
     define("_MESSLIVE_LOGIN","Melden Sie sich bei {$_FN['sitename']} mit Messenger Live Connect");
